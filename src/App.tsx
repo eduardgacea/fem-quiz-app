81f15137bf6f94ac4b5015ee5a6f2a72e2bec1b5
eduardgacea <eduardgacea@gmail.com>
2024-04-29 12:48:22 +0300

    add mock styles for testing new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
 import ResizeListener from "./components/ResizeListener";
 import GlobalStyles from "./styles/GlobalStyles";
+import Backdrop from "./ui/Backdrop";
+import Option from "./ui/Option";
+
+import { quizzes } from "./data/data.json";
 
 function App() {
     return (
-        <>
+        <div
+            style={{
+                display: "flex",
+                background: "transparent",
+                height: "100dvh",
+                alignItems: "center",
+                justifyContent: "center",
+                padding: "0 24px",
+            }}
+        >
             <GlobalStyles />
             <ResizeListener />
-        </>
+            <Backdrop />
+            <div style={{ display: "flex", flexDirection: "column", gap: "1rem", width: "100%" }}>
+                {quizzes.map(quiz => (
+                    <Option key={quiz.id} type="subject" icon={`icon-${quiz.title.toLowerCase()}.svg`}>
+                        {quiz.title}
+                    </Option>
+                ))}
+            </div>
+        </div>
     );
 }
 
